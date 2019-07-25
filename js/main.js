204957f7ca42850abd608355947593d06e3de5e6
Megan Fonde <meganfonde@gmail.com>
2019-07-25 09:52:49 -0400

    updated resume to PDF

---
+++ js/main.js
@@ -0,0 +1,25 @@
+let beyonce = document.getElementById('beyonce');
+
+setTimeout (
+    () => {
+        beyonce.innerHTML = `Einstein'`
+    }, 3000
+)
+
+setTimeout (
+    () => {
+        beyonce.innerHTML = `Michael Jordan'`
+    }, 6000
+)
+
+setTimeout (
+    () => {
+        beyonce.innerHTML = `Steve Jobs'`
+    }, 9000
+)
+
+setTimeout (
+    () => {
+        beyonce.innerHTML = `Michelle Obama'`
+    }, 12000
+)
