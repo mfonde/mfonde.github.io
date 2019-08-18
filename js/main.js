87e67861c1ffad13b5bb9b93124aedc533ccb0aa
Megan Fonde <meganfonde@gmail.com>
2019-08-18 18:58:52 -0400

    spacing changes

--- js/main.js
+++ js/main.js
@@ -1,25 +1,25 @@
-let beyonce = document.getElementById('beyonce');
+// let beyonce = document.getElementById('beyonce');
 
-setTimeout (
-    () => {
-        beyonce.innerHTML = `Einstein'`
-    }, 3000
-)
+// setTimeout (
+//     () => {
+//         beyonce.innerHTML = `Einstein'`
+//     }, 3000
+// )
 
-setTimeout (
-    () => {
-        beyonce.innerHTML = `Michael Jordan'`
-    }, 6000
-)
+// setTimeout (
+//     () => {
+//         beyonce.innerHTML = `Michael Jordan'`
+//     }, 6000
+// )
 
-setTimeout (
-    () => {
-        beyonce.innerHTML = `Steve Jobs'`
-    }, 9000
-)
+// setTimeout (
+//     () => {
+//         beyonce.innerHTML = `Steve Jobs'`
+//     }, 9000
+// )
 
-setTimeout (
-    () => {
-        beyonce.innerHTML = `Michelle Obama'`
-    }, 12000
-)
+// setTimeout (
+//     () => {
+//         beyonce.innerHTML = `Michelle Obama'`
+//     }, 12000
+// )
